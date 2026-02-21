2c78804974a3dc1bc55444dbd22ed4f2717fd345
Ryan Alamo <ralamo33@gmail.com>
2026-02-21 08:48:16 -0500

    I have data connection

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,18 @@
+import { createServerSupabase } from "@/lib/supabase/server";
+
 export default async function NamePage() {
+    const supabase = await createServerSupabase();
+    const { data: faces } = await supabase.from("face").select("*").limit(100);
+
+    if (!faces) {
+        return <p>Something went wrong!</p>;
+    }
+
     return (
         <div>
-            <p>Start the name game</p>
+            {faces.map((f, idx) => {
+                return <p key={`Face ${f.id} ${idx}`}>{f.name}</p>;
+            })}
         </div>
     );
 }
